0fa40d14194aa6b679809bfe6e36fc0ec865bc0b
vvv <nmurge123@gmail.com>
2024-08-17 14:53:19 +0530

    load

--- Koli.java
+++ Koli.java
@@ -4,6 +4,11 @@ public class Koli {
 
 	public static void main(String[] args) {
 		system.out.println("hello");
+
+         Thread.sleep(8000);
+driver.close();
+          driver.quit();
+
 	}
 
 }
